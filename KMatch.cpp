575ed9d362ace3990dd46781219877b38c20a1cc
mzy8329 <mzy8329@163.com>
2025-03-27 11:41:11 +0800

    add Hungarian

--- KMatch.cpp
+++ KMatch.cpp
@@ -1,6 +1,12 @@
 #include "KMatch.h"
 
-bool rDepthFirstSearch(int _i,
+typedef enum {
+    KM,
+    Hungarian
+}Mtype_e;
+
+bool rDepthFirstSearch(const Mtype_e& _type,
+    int _i,
     Eigen::MatrixXf& _adj_matrix,
     std::vector<float>& _X_label,
     std::vector<float>& _Y_label,
@@ -11,52 +17,70 @@ bool rDepthFirstSearch(int _i,
 {
     _X_visited[_i] = 1;
 
-    for (int j = 0; j < _adj_matrix.cols(); j++)
-    {
-        if (_Y_visited[j] == 1 || _adj_matrix(_i, j) == 0)
-        {
-            continue;
+    if (_type == KM) {
+        for (int j = 0; j < _adj_matrix.cols(); j++) {
+            if (_Y_visited[j] == 1 || _adj_matrix(_i, j) == 0) {
+                continue;
+            }
+
+            if (_X_label[_i] + _Y_label[j] == _adj_matrix(_i, j)) {
+                _Y_visited[j] = 1;
+
+                if (_Y_match_index[j] == -1 ||
+                    rDepthFirstSearch(_type,
+                        _Y_match_index[j],
+                        _adj_matrix,
+                        _X_label,
+                        _Y_label,
+                        _Y_match_index,
+                        _X_visited,
+                        _Y_visited,
+                        _minimum_d)) {
+                    _Y_match_index[j] = _i;
+                    return 1;
+                }
+            }
+            else {
+                _minimum_d[j] = std::min(_minimum_d[j], _X_label[_i] + _Y_label[j] - _adj_matrix(_i, j));
+            }
         }
+    }
+    else {
+        for (int j = 0; j < _adj_matrix.cols(); j++) {
+            if (_Y_visited[j] == 1 || _adj_matrix(_i, j) == 0) {
+                continue;
+            }
 
-        if (_X_label[_i] + _Y_label[j] == _adj_matrix(_i, j))
-        {
             _Y_visited[j] = 1;
-
             if (_Y_match_index[j] == -1 ||
-                rDepthFirstSearch(_Y_match_index[j],
+                rDepthFirstSearch(_type,
+                    _Y_match_index[j],
                     _adj_matrix,
                     _X_label,
                     _Y_label,
                     _Y_match_index,
                     _X_visited,
                     _Y_visited,
-                    _minimum_d))
-            {
+                    _minimum_d)) {
                 _Y_match_index[j] = _i;
                 return 1;
             }
         }
-        else
-        {
-            _minimum_d[j] = std::min(_minimum_d[j], _X_label[_i] + _Y_label[j] - _adj_matrix(_i, j));
-        }
     }
+
 
     return 0;
 }
 
 std::vector<int> KMatch(Eigen::MatrixXf _adj_matrix, int _type, float _disconnect_value)
 {
-    if (_type == LOSS)
-    {
+    if (_type == LOSS) {
         float max_element = _adj_matrix.maxCoeff();
         _adj_matrix *= -1;
-        if (std::abs(max_element - _disconnect_value) < 0.01f)
-        {
+        if (std::abs(max_element - _disconnect_value) < 0.01f) {
             _adj_matrix = _adj_matrix.array() + max_element;
         }
-        else
-        {
+        else {
             _adj_matrix = _adj_matrix.array() + max_element + 1;
         }
     }
@@ -70,8 +94,7 @@ std::vector<int> KMatch(Eigen::MatrixXf _adj_matrix, int _type, float _disconnec
     std::vector<float> X_label(size, 0.0);
     std::vector<float> Y_label(size, 0.0);
 
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         Eigen::VectorXf row_vec = adj_matrix.row(i);
         X_label[i] = row_vec.maxCoeff();
     }
@@ -80,54 +103,73 @@ std::vector<int> KMatch(Eigen::MatrixXf _adj_matrix, int _type, float _disconnec
 
     std::vector<int> X_visited(size, 0);
     std::vector<int> Y_visited(size, 0);
-    std::vector<float> minimum_d(size, 65536.0);
-
-    for (int i = 0; i < size; i++)
-    {
-        while (1)
-        {
+    std::vector<float> minimum_d(size, MATCH_INF);
+    for (int i = 0; i < size; i++) {
+        while (1) {
             std::fill(X_visited.begin(), X_visited.end(), 0);
             std::fill(Y_visited.begin(), Y_visited.end(), 0);
-            std::fill(minimum_d.begin(), minimum_d.end(), 65536.0);
+            std::fill(minimum_d.begin(), minimum_d.end(), MATCH_INF);
 
-            if (rDepthFirstSearch(i,
+            if (rDepthFirstSearch(KM,
+                i,
                 adj_matrix,
                 X_label,
                 Y_label,
                 Y_match_index,
                 X_visited,
                 Y_visited,
-                minimum_d))
-            {
+                minimum_d)) {
                 break;
             }
 
-            float delta = 65536.0;
-            for (int j = 0; j < size; j++)
-            {
+            float delta = MATCH_INF;
+            for (int j = 0; j < size; j++) {
                 delta = std::min(delta, minimum_d[j]);
             }
 
-            // 没有可增加的边
-            if (std::abs(delta - 65536.0) < 1.0 || std::abs(delta) < 1e-3)
-            {
+            // no edge
+            if (std::abs(delta - MATCH_INF) < 1.0 || std::abs(delta) < 1e-3) {
                 break;
             }
 
-            // 加边
-            for (int n = 0; n < size; n++)
-            {
-                if (X_visited[n])
-                {
+            // add edge
+            for (int n = 0; n < size; n++) {
+                if (X_visited[n]) {
                     X_label[n] -= delta;
                 }
-                if (Y_visited[n])
-                {
+                if (Y_visited[n]) {
                     Y_label[n] += delta;
                 }
             }
         }
     }
+    return Y_match_index;
+}
 
+
+std::vector<int> HungarianMatch(Eigen::MatrixXf _adj_matrix)
+{
+    int size = std::max(_adj_matrix.rows(), _adj_matrix.cols());
+    Eigen::MatrixXf adj_matrix(size, size);
+    adj_matrix.fill(-1);
+    adj_matrix.block(0, 0, _adj_matrix.rows(), _adj_matrix.cols()) = _adj_matrix;
+
+    std::vector<int> Y_match_index(size, -1);
+    std::vector<int> X_visited(size, 0);
+    std::vector<int> Y_visited(size, 0);
+    std::vector<float> place_holder;
+    for (int i = 0; i < size; i++) {
+        if (rDepthFirstSearch(Hungarian,
+            i,
+            adj_matrix,
+            place_holder,
+            place_holder,
+            Y_match_index,
+            X_visited,
+            Y_visited,
+            place_holder)) {
+            break;
+        }
+    }
     return Y_match_index;
 }
--- KMatch.h
+++ KMatch.h
@@ -8,6 +8,7 @@
 
 #define REWARD 0
 #define LOSS 1
+#define MATCH_INF 65535.0 // the max value in adjacency matrix should less than MATCH_INF
 
 /**
  * @brief get match by KM algorithm
@@ -16,5 +17,7 @@
  * @return std::vector<int>: return col-based matching result
  */
 std::vector<int> KMatch(Eigen::MatrixXf _adj_matrix, int _type, float _disconnect_value);
+
+std::vector<int> HungarianMatch(Eigen::MatrixXf _adj_matrix, float _disconnect_value);
 
 #endif
